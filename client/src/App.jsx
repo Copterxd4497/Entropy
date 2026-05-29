f65e1d707a97520fe20a36bd4c4777121da43ceb
Copter Panyaphon <copterpanyphon@gmail.com>
2026-05-29 13:02:14 +0700

    direct HomePage page to /HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import Something from "./pages/something";
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Something} />
+        <Route path="/HomePage" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/something.jsx
@@ -0,0 +1,8 @@
+export default function Something() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h1>Fuck you bitch </h1>
+    </>
+  );
+}
